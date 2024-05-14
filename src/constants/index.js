ad8e808271bd0012fc8bb13c500a55d5541e75b8
San Shwe <sanxhwe@gmail.com>
2024-05-14 13:48:00 +0630

    constant

---
+++ src/constants/index.js
@@ -0,0 +1,67 @@
+export const navigation = [
+    {
+      id: "0",
+      title: "Team 7",
+      url: "#teamseven",
+    },
+    {
+      id: "1",
+      title: "Team 3",
+      url: "#teamthree",
+    },
+    {
+      id: "2",
+      title: "Team 8",
+      url: "#teameight",
+    },
+    {
+      id: "3",
+      title: "Team 10",
+      url: "#teamten",
+    },
+    {
+      id: "4",
+      title: "New account",
+      url: "#signup",
+      onlyMobile: true,
+    },
+    {
+      id: "5",
+      title: "Sign in",
+      url: "#login",
+      onlyMobile: true,
+    },
+  ];
+
+  export const socials = [
+    {
+      id: "0",
+      title: "Discord",
+      iconUrl: discordBlack,
+      url: "#",
+    },
+    {
+      id: "1",
+      title: "Twitter",
+      iconUrl: twitter,
+      url: "#",
+    },
+    {
+      id: "2",
+      title: "Instagram",
+      iconUrl: instagram,
+      url: "#",
+    },
+    {
+      id: "3",
+      title: "Telegram",
+      iconUrl: telegram,
+      url: "#",
+    },
+    {
+      id: "4",
+      title: "Facebook",
+      iconUrl: facebook,
+      url: "#",
+    },
+  ];
