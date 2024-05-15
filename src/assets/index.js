cd3aa1ad4cb7efdf633fe2da4cab808754d19bc1
San Shwe <sanxhwe@gmail.com>
2024-05-15 12:10:16 +0630

    Hero Section

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,24 @@
 import naruto from "./naruto.png";
 import background from "./background.jpg";
+import hero from "./Hero/hero.jpg";
+import curve from "./Hero/curve.png";
+import herobackground from "./Hero/herobackground.jpg";
+import homeSmile from "./home-smile.svg";
+import file02 from "./file-02.svg";
+import searchMd from "./search-md.svg";
+import plusSquare from "./plus-square.svg";
+import loading from "./loading.png";
+
 
 export {
     naruto,
     background,
+    hero,
+    herobackground,
+    curve,
+    homeSmile,
+    file02,
+    searchMd,
+    plusSquare,
+    loading
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,99 @@
+import { hero, herobackground, curve } from "../assets"
+
+import Button from "./Button";
+import Section from "./Section";
+
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
+import Konoha from "./Konoha";
+
 
 const Hero = () => {
+    const parallaxRef = useRef(null);
+
   return (
-    <div>Hero</div>
+    <Section  className="pt-[12rem] -mt-[5.25rem]"
+    crosses
+    crossesOffset="lg:translate-y-[5.25rem]"
+    customPaddings
+    id="hero">
+         <div className="container relative" ref={parallaxRef}>
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
+          <h1 className="h1 mb-6 ">
+            Welcome to the world of {` `}
+            <span className="inline-block relative text-color-7">
+              Konoha{" "}
+              <img
+                src={curve}
+                className="absolute top-full left-0 w-full xl:-mt-2"
+                width={624}
+                height={28}
+                alt="Curve"
+              />
+            </span>
+          </h1>
+          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-1 lg:mb-8">
+          Dive into the heart of the Hidden Leaf Village and meet the legendary teams that have shaped its history. 
+          </p>
+          <Button href="/#teamseven" white>
+            Get started
+          </Button>
+        </div>
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+            <div className="relative bg-n-8 rounded-[1rem]">
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                <img
+                  src={hero}
+                  className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
+                  width={1024}
+                  height={490}
+                  alt="hero"
+                />
+
+                <ScrollParallax isAbsolutelyPositioned>
+                    <Konoha className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2 text-n-1" />
+                </ScrollParallax>
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} width={24} height={25} alt={icon} />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
+
+                
+              </div>
+            </div>
+
+            <Gradient />
+          </div>
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 max-md:-top-[67%] max-sm:-top-[75%] md:w-[138%] lg:-top-[85.5%]">
+            <img
+              src={herobackground}
+              className="w-full"
+              width={1440}
+              height={1800}
+              alt="hero"
+            />
+          </div>
+
+          <BackgroundCircles />
+        </div>
+
+       
+      </div>
+
+      <BottomLine />
+    
+    </Section>
   )
 }
 
