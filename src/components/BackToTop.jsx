7ef3343b74aed4762fdb9e4b5559ea50507ec669
San Shwe <sanxhwe@gmail.com>
2024-05-18 16:30:40 +0630

    BackToTop Button

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -29,7 +29,7 @@ const BackToTopButton = () => {
     <button
       type="button"
       onClick={scrollToTop}
-      className={`fixed bottom-5 right-5 p-3 rounded-full bg-color-9 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-opacity duration-300 z-50 ${
+      className={`fixed bottom-5 right-1.5 p-3 rounded-full bg-color-9 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-opacity duration-300 z-50 ${
         visible ? "opacity-100" : "opacity-0"
       }`}
     >
